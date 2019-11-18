7e65952814d750300f1d932d4c15f88a3df0c6a4
Agustín Ibars <agustin.ibars@gmail.com>
2019-11-18 20:45:39 -0300

    Add advanced react initial course files

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = props => {
+    <div>
+        <p>Hey</p>
+        <Link href="/sell">
+            <a>Sell</a>
+        </Link>
+    </div>
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => {
+    <div>
+        <p>sell</p>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </div>
+}
+
+export default Sell;
