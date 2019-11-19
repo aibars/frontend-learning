925acc4781f763ecdb28627c2173770b696d1e18
Agustín Ibars <agustin.ibars@gmail.com>
2019-11-18 21:02:35 -0300

    initial boilerplate files

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
+ 
 import Link from 'next/link';
 
-const Home = props => {
-    <div>
-        <p>Hey</p>
-        <Link href="/sell">
-            <a>Sell</a>
-        </Link>
-    </div>
-}
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+  </div>
+);
 
 export default Home;
