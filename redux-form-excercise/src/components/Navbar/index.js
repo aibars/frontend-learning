7eac7b183b6ba0435e5e2d32b7beb8c7301a2ae3
Agustín Ibars <agustin.ibars@gmail.com>
2020-10-16 08:28:12 -0300

    remove unused code

--- redux-form-excercise/src/components/Navbar/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar = () => (
-  <div className="navbar">
-    <span className="navbar__label">Simple Website Builder</span>
-  </div>
-);
-
-export default Navbar;
